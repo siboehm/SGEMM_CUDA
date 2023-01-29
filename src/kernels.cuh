d1923ee910829a7ff2029729bf9b00c90af7c3f6
Simon Boehm <simonboehm@mailbox.org>
2023-01-29 22:26:36 +0000

    Fix import after rename

--- src/kernels.cuh
+++ src/kernels.cuh
@@ -8,4 +8,4 @@
 #include "kernels/6_kernel_vectorize.cuh"
 #include "kernels/7_kernel_resolve_bank_conflicts.cuh"
 #include "kernels/8_kernel_bank_extra_col.cuh"
-#include "kernels/9_kernel_generalizing.cuh"
+#include "kernels/9_kernel_autotuned.cuh"
