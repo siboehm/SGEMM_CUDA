eef5ec3b44360d8e832d5a269bd905a3e8cfbd2d
Simon Boehm <simonboehm@mailbox.org>
2023-02-05 00:12:37 +0000

    K9: Implement warptiling

--- src/kernels/9_kernel_autotuned.cuh
+++ src/kernels/9_kernel_autotuned.cuh
@@ -8,23 +8,25 @@
 #include <cuda_runtime.h>
 
 #define CEIL_DIV(M, N) ((M) + (N)-1) / (N)
+const int NUM_THREADS = 256;
 
 template <const int BM, const int BN, const int BK, const int TM, const int TN>
-__global__ void sgemmAutotuned(int M, int N, int K, float alpha, float *A,
-                               float *B, float beta, float *C) {
+__global__ void __launch_bounds__(NUM_THREADS)
+    sgemmAutotuned(int M, int N, int K, float alpha, float *A, float *B,
+                   float beta, float *C) {
   const uint cRow = blockIdx.y;
   const uint cCol = blockIdx.x;
 
-  constexpr uint totalResultsBlocktile = BM * BN;
-  // A thread is responsible for calculating TM*TN elements in the blocktile
-  constexpr uint numThreadsBlocktile = totalResultsBlocktile / (TM * TN);
+  // size of warptile
+  constexpr int WM = TM * 16;
+  constexpr int WN = TN * 16;
+  // iterations of warptile
+  constexpr int WMITER = CEIL_DIV(BM, WM);
+  constexpr int WNITER = CEIL_DIV(BN, WN);
 
-  // ResultsPerBlock / ResultsPerThread == ThreadsPerBlock
-  assert(numThreadsBlocktile == blockDim.x);
-
-  // BN/TN are the number of threads to span a column
-  const int threadCol = threadIdx.x % (BN / TN);
-  const int threadRow = threadIdx.x / (BN / TN);
+  // Placement of the thread in the warptile
+  const int threadCol = threadIdx.x % (WN / TN);
+  const int threadRow = threadIdx.x / (WN / TN);
 
   // allocate space for the current blocktile in smem
   __shared__ float As[BM * BK];
@@ -39,14 +41,13 @@ __global__ void sgemmAutotuned(int M, int N, int K, float alpha, float *A,
   // we'll load 128bit / 32bit = 4 elements per thread at each step
   const uint innerRowA = threadIdx.x / (BK / 4);
   const uint innerColA = threadIdx.x % (BK / 4);
-  constexpr uint rowStrideA = (numThreadsBlocktile * 4) / BK;
-
+  constexpr uint rowStrideA = (NUM_THREADS * 4) / BK;
   const uint innerRowB = threadIdx.x / (BN / 4);
   const uint innerColB = threadIdx.x % (BN / 4);
-  constexpr uint rowStrideB = numThreadsBlocktile / (BN / 4);
+  constexpr uint rowStrideB = NUM_THREADS / (BN / 4);
 
   // allocate thread-local cache for results in registerfile
-  float threadResults[TM * TN] = {0.0};
+  float threadResults[WMITER * WNITER * TM * TN] = {0.0};
   float regM[TM] = {0.0};
   float regN[TN] = {0.0};
 
@@ -75,19 +76,24 @@ __global__ void sgemmAutotuned(int M, int N, int K, float alpha, float *A,
     A += BK;     // move BK columns to right
     B += BK * N; // move BK rows down
 
-    // calculate per-thread results
-    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
-      // block into registers
-      for (uint i = 0; i < TM; ++i) {
-        regM[i] = As[dotIdx * BM + threadRow * TM + i];
-      }
-      for (uint i = 0; i < TN; ++i) {
-        regN[i] = Bs[dotIdx * BN + threadCol * TN + i];
-      }
-      for (uint resIdxM = 0; resIdxM < TM; ++resIdxM) {
-        for (uint resIdxN = 0; resIdxN < TN; ++resIdxN) {
-          threadResults[resIdxM * TN + resIdxN] +=
-              regM[resIdxM] * regN[resIdxN];
+    for (uint wmIdx = 0; wmIdx < WMITER; ++wmIdx) {
+      for (uint wnIdx = 0; wnIdx < WNITER; ++wnIdx) {
+        // calculate per-thread results
+        for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
+          // block into registers
+          for (uint i = 0; i < TM; ++i) {
+            regM[i] = As[dotIdx * BM + (wmIdx * WM) + threadRow * TM + i];
+          }
+          for (uint i = 0; i < TN; ++i) {
+            regN[i] = Bs[dotIdx * BN + (wnIdx * WN) + threadCol * TN + i];
+          }
+          for (uint resIdxM = 0; resIdxM < TM; ++resIdxM) {
+            for (uint resIdxN = 0; resIdxN < TN; ++resIdxN) {
+              threadResults[(wmIdx * TM + resIdxM) * (WNITER * TN) +
+                            wnIdx * TN + resIdxN] +=
+                  regM[resIdxM] * regN[resIdxN];
+            }
+          }
         }
       }
     }
@@ -95,20 +101,28 @@ __global__ void sgemmAutotuned(int M, int N, int K, float alpha, float *A,
   }
 
   // write out the results
-  for (uint resIdxM = 0; resIdxM < TM; resIdxM += 1) {
-    for (uint resIdxN = 0; resIdxN < TN; resIdxN += 4) {
-      // load C vector into registers
-      float4 tmp = reinterpret_cast<float4 *>(
-          &C[(threadRow * TM + resIdxM) * N + threadCol * TN + resIdxN])[0];
-      // perform GEMM update in reg
-      tmp.x = alpha * threadResults[resIdxM * TN + resIdxN] + beta * tmp.x;
-      tmp.y = alpha * threadResults[resIdxM * TN + resIdxN + 1] + beta * tmp.y;
-      tmp.z = alpha * threadResults[resIdxM * TN + resIdxN + 2] + beta * tmp.z;
-      tmp.w = alpha * threadResults[resIdxM * TN + resIdxN + 3] + beta * tmp.w;
-      // write back
-      reinterpret_cast<float4 *>(
-          &C[(threadRow * TM + resIdxM) * N + threadCol * TN + resIdxN])[0] =
-          tmp;
+  for (uint wmIdx = 0; wmIdx < WMITER; ++wmIdx) {
+    for (uint wnIdx = 0; wnIdx < WNITER; ++wnIdx) {
+      float *C_interim = C + (wmIdx * WM * N) + (wnIdx * WN);
+      for (uint resIdxM = 0; resIdxM < TM; resIdxM += 1) {
+        for (uint resIdxN = 0; resIdxN < TN; resIdxN += 4) {
+          // load C vector into registers
+          float4 tmp = reinterpret_cast<float4 *>(
+              &C_interim[(threadRow * TM + resIdxM) * N + threadCol * TN +
+                         resIdxN])[0];
+          // perform GEMM update in reg
+          const int i =
+              (wmIdx * TM + resIdxM) * (WNITER * TN) + wnIdx * TN + resIdxN;
+          tmp.x = alpha * threadResults[i + 0] + beta * tmp.x;
+          tmp.y = alpha * threadResults[i + 1] + beta * tmp.y;
+          tmp.z = alpha * threadResults[i + 2] + beta * tmp.z;
+          tmp.w = alpha * threadResults[i + 3] + beta * tmp.w;
+          // write back
+          reinterpret_cast<float4 *>(&C_interim[(threadRow * TM + resIdxM) * N +
+                                                threadCol * TN + resIdxN])[0] =
+              tmp;
+        }
+      }
     }
   }
 }
